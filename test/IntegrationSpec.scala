b172f2981a822863cb898125a5ffe50c89a00201
Yao Wenchi <wenchi_yao@yahoo.com>
2018-05-20 22:32:34 +0800

    small refactoring on IT

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -4,11 +4,10 @@ import play.api.libs.json.Json
 import play.api.test._
 
 class IntegrationSpec extends PlaySpecification {
-  private val port: Int = 9000
   "Application" should {
     // Test application readiness
     "be reachable" in new WithApplication {
-      val home = route(app, FakeRequest(GET, "/")).get
+      private val home = route(app, FakeRequest(GET, "/")).get
       status(home) must equalTo(OK)
       contentAsString(home) must contain ("Your new application is ready.")
     }
@@ -21,42 +20,41 @@ class IntegrationSpec extends PlaySpecification {
     // Test User Wallet API
     // 0 is not a valid wallet id
     "Get 'wallet not found' response with unknown wallet" in new WithApplication {
-      val wallet = route(app, FakeRequest(GET, "/wallet/0")).get
+      private val wallet = route(app, FakeRequest(GET, "/wallet/0")).get
       status(wallet) must equalTo(OK)
       contentAsString(wallet) mustEqual "Wallet not found"
     }
 
     // 1 is chosen since company id always exists
     "Get the right wallet with the right id" in new WithApplication {
-      val wallet = route(app, FakeRequest(GET, "/wallet/1")).get
+      private val wallet = route(app, FakeRequest(GET, "/wallet/1")).get
       status(wallet) must equalTo(OK)
-      val walletJson = contentAsJson(wallet)
-      (walletJson \ "id").get.toString mustEqual("1")
+      private val walletJson = contentAsJson(wallet)
+      (walletJson \ "id").get.toString mustEqual "1"
       (walletJson \ "balance").isDefined must beTrue
       (walletJson \ "currency").isDefined must beTrue
     }
 
     // Test purchase product API
     "Successfully make purchase with product id 3" in new WithApplication {
-      val purchaseStatus =
+      private val purchaseStatus =
         route(app, FakeRequest.apply(POST, "/products/3/purchase", FakeHeaders(Seq(HeaderNames.HOST -> "localhost")), Json.toJson(User(3)))).get
       status(purchaseStatus) must equalTo(OK)
       contentAsString(purchaseStatus) mustEqual "Purchase Successful"
     }
 
     "Failure to make purchase with wrong wallet id" in new WithApplication {
-      val purchaseStatus = route(app, FakeRequest.apply(POST, "/products/3/purchase", FakeHeaders(Seq(HeaderNames.HOST -> "localhost")), Json.toJson(User(0)))).get
+      private val purchaseStatus =
+        route(app, FakeRequest.apply(POST, "/products/3/purchase", FakeHeaders(Seq(HeaderNames.HOST -> "localhost")), Json.toJson(User(0)))).get
       status(purchaseStatus) must equalTo(OK)
       contentAsString(purchaseStatus) mustEqual "Purchase Failed"
     }
-
-
+    
     // Test Get Current Flash Sale API
     "Get some flash sales data" in new WithApplication {
-      val sales = route(app, FakeRequest(GET, "/sales/current?country=SG")).get
+      private val sales = route(app, FakeRequest(GET, "/sales/current?country=SG")).get
       status(sales) must equalTo(OK)
-      val salesJson = contentAsJson(sales)
-      (salesJson \ "sales").as[Seq[ProductSale]].size > 0 must beTrue
+      (contentAsJson(sales) \ "sales").as[Seq[ProductSale]].nonEmpty must beTrue
     }
   }
 }
